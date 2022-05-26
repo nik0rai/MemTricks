bbea252974f32808eb44b4bd0307c426ff6db920
nik0rai <nikolay-sviridov@inbox.ru>
2022-05-26 23:38:23 +0300

    Update Stack.h

--- Headers/Stack.h
+++ Headers/Stack.h
@@ -1,43 +1,47 @@
-#include "Tester.h"
 #include "Allocator.h"
 
-#pragma once
-template<typename T>
+template <class T>
 class Stack
 {
-private:
-	T top;
-	T* items;
 public:
-	int maxSize; //max capacity of stack
+	struct Node
+	{
+		T data;
+		Node* next;
+	};
+	Node* head;
+	Allocator* allocator;
 
-	void init(int capa) {
-		maxSize = capa;
-		top = -1;
-		Allocator* al = new Allocator(maxSize);
-		al->init();
-		items = (T*)al->alloc(sizeof(T) * capa, 8);
 
-		//items = (T*)malloc(sizeof(T) * capa); //default malloc
+	Stack() {
+		allocator = new Allocator(4096, 8);
 	}
-	int size() { return top + 1; }
-	bool isEmpty() { return size() == 0; }
-	bool isFull() { return top == (maxSize - 1); }
-	void push(T value) {
-		if (!isFull()) {
-			top++;
-			items[top] = value;
+
+	void Push(T data)
+	{
+		if (head == NULL)
+		{
+			Node* nNode = (Node*)allocator->Allocate(sizeof(data));
+			head = nNode;
 		}
-	}
-	T getTop() { return (!isEmpty()) ? items[top] : -1; } //-1 means stack was actually empty
-	T pop() {
-		if (!isEmpty()) {
-			top--;
-			return items[top];
+		else
+		{
+			Node* nNode = (Node*)allocator->Allocate(sizeof(data));
+			nNode->next = head;
+			head = nNode;
 		}
+		head->data = data;
 	}
-	void print() {
-		for (int i = maxSize - 1; i >= 0; i--)
-			std::cout << items[i] << std::endl;
+
+	T Pop()
+	{
+		if (head == NULL)
+			return NULL;
+		Node* temp;
+		temp = head;
+		head = head->next;
+		allocator->Free((void*)temp);
+		return temp->data;
 	}
+
 };
