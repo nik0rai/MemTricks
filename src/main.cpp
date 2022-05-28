39c8645770fd00bb16458b3b164e73a41fdff90c
44x44 <71344403+44x44@users.noreply.github.com>
2022-05-28 10:57:49 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,31 @@
 #include <string>
 
 #include "Allocator.h"
-#include "Stack.h"
+//#include "Stack.h"
 #include "HashTable.h"
+#include "ReversePolishNotation.h"
 
 int main()
 {
-	HashTable<std::string> h(4);
+  std::string str = "(x - y) * 4 - z * k";
+    ReversePolishNotation* reversePolishNotation = new ReversePolishNotation();
+    std::string srpn = reversePolishNotation->RPN(str);
+    std::cout << "Reverse Polish notation: " << srpn << std::endl;
+    return 0;
+}
+
+
+
+
+
+
+
+/*
+HashTable<std::string> h(4);
 	h.insert("ananas");
 	h.insert("banan");
 	h.insert("persik");
 	h.insert("petrushka");
 	h.print();
 	return 0;
-}
+*/
